da88217a07afcb943418ad582a1bc1f15a9f64e8
Javier Segura <javier@miolabs.com>
2024-02-06 01:17:06 -0800

    adding objc support

--- Plugins/MIOCoreDataBuildModelPlugin/MIOCoreDataBuildModelPlugin.swift
+++ Plugins/MIOCoreDataBuildModelPlugin/MIOCoreDataBuildModelPlugin.swift
@@ -7,10 +7,10 @@ struct MIOCoreDataBuildModelPlugin: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
         // This plugin only runs for package targets that can have source files.
 //        guard let sourceFiles = target.sourceModule?.sourceFiles else { return [] }
-
+        
         // Find the code generator tool to run (replace this with the actual one).
-//        let generatorTool = try context.tool(named: "DataModelBuilder")
-
+//        let generatorTool = try context.tool( named: "miotool" )
+        
         // Construct a build command for each source file with a particular suffix.
 //        return sourceFiles.map(\.path).compactMap {
 //            createBuildCommand(for: $0, in: context.pluginWorkDirectory, with: generatorTool.path)
@@ -27,10 +27,10 @@ extension MIOCoreDataBuildModelPlugin: XcodeBuildToolPlugin {
     func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
         // Find the code generator tool to run (replace this with the actual one).
         let generatorTool = try context.tool(named: "miotool")
-
+                
         // Construct a build command for each source file with a particular suffix.
         return target.inputFiles.map(\.path).compactMap {
-            createBuildCommand(for: $0, in: context.pluginWorkDirectory, with: generatorTool.path)
+            createBuildCommand(for: $0, in: context.pluginWorkDirectory, with: generatorTool.path, configPath: context.xcodeProject.directory )
         }
     }
 }
@@ -39,15 +39,24 @@ extension MIOCoreDataBuildModelPlugin: XcodeBuildToolPlugin {
 
 extension MIOCoreDataBuildModelPlugin {
     /// Shared function that returns a configured build command if the input files is one that should be processed.
-    func createBuildCommand(for inputPath: Path, in outputDirectoryPath: Path, with generatorToolPath: Path) -> Command? {
+    func createBuildCommand(for inputPath: Path, in outputDirectoryPath: Path, with generatorToolPath: Path, configPath: Path? ) -> Command? {
         // Skip any file that doesn't have the extension we're looking for (replace this with the actual one).
         guard inputPath.extension == "xcdatamodeld" else { return .none }
                 
+//        let path = configPath.appending( subpath: "mio_plugins.json" )
+//        print("Search for plugin configuration file at \(path)" )
+//        let data = try? Data(contentsOf: path )
+        var arguments = ["model", "generate", "\(inputPath)", "-o", "\(outputDirectoryPath)", "--swift"]
+        
+        //#if os(iOS)
+        arguments.append( "--objc-support" )
+        //#endif
+        
         // Return a command that will run during the build to generate the output file.
         return .prebuildCommand(
             displayName: "Generating model classes",
             executable: generatorToolPath,
-            arguments: ["model", "generate", "\(inputPath)", "-o", "\(outputDirectoryPath)", "--swift", "--objc-support"],
+            arguments: arguments,
             outputFilesDirectory: outputDirectoryPath
         )
     }
